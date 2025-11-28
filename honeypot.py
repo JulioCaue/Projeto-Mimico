59f57e1a9132684acd8b9e54ce068ddd0064fef5
JulioCaue <juliocaue22@gmail.com>
2025-11-28 02:28:05 -0300

    Nova versão do servidor, menos bugs, continua ligado infinitamente.

--- honeypot.py
+++ honeypot.py
@@ -11,41 +11,54 @@ class Honeypot:
         self.servidor.listen()
 
     def rodar_honeypot(self):
-        print('Esperando Conexão...')
-        socket_comunicação,endereço=self.servidor.accept()
-        usuario_tentado='null'
-        senha_tentada='null'
-        print(f'conexão estabelecida com endereço {endereço}.')
-        socket_comunicação.send('220 welcome'.encode())
         while  True:
-            lista_de_endereços=[]
             if os.path.exists('honeypot.log'):
+                lista_de_endereços=[]
                 with open ('Honeypot.log','r') as log:
                     for linha in log:
                         lista_de_endereços.append(linha.strip())
-
-            recebida=socket_comunicação.recv(1024).decode()
-            if recebida!=('quit'):
-                if recebida.startswith('user'):
-                    usuario_tentado=recebida
-                    socket_comunicação.send('331'.encode())
-                elif recebida.startswith('pass'):
-                    senha_tentada=recebida
-                    socket_comunicação.send('530'.encode())
-                else:
-                    socket_comunicação.send('500'.encode())
-                
             else:
-                socket_comunicação.send('221'.encode())
-                acessos=f'[{datetime.datetime.now()}]: [{endereço}] - [Usuario tentado: {usuario_tentado}] - [Senha tentada: {senha_tentada}]'
-                lista_de_endereços.append(acessos)
-                if senha_tentada != 'null' and usuario_tentado != 'null':
-                    with open ('Honeypot.log','w') as log:
-                        for endereços in lista_de_endereços:
-                            log.write (f'{endereços}\n')
-                socket_comunicação.close()
-                print (f'conexão encerrada com {endereço}')
-                break
+                lista_de_endereços=[]
+            
+            print('Esperando Conexão...')
+            socket_comunicação,endereço=self.servidor.accept()
+            usuario_tentado='null'
+            senha_tentada='null'
+            print(f'conexão estabelecida com endereço {endereço}.')
+            socket_comunicação.send('220 welcome'.encode())
+            pass
+
+            try:
+                while True:
+                    recebida=socket_comunicação.recv(1024).decode()
+                    if not recebida==False:
+                        if recebida!=('quit'):
+                            if recebida.startswith('user'):
+                                usuario_tentado=recebida
+                                socket_comunicação.send('331'.encode())
+                            elif recebida.startswith('pass'):
+                                senha_tentada=recebida
+                                socket_comunicação.send('530'.encode())
+                                acessos=f'[{datetime.datetime.now()}]: [{endereço}] - [Usuario tentado: {usuario_tentado}] - [Senha tentada: {senha_tentada}]'
+                                lista_de_endereços.append(acessos)
+                                if senha_tentada != 'null' and usuario_tentado != 'null':
+                                    with open ('Honeypot.log','w') as log:
+                                        for endereços in lista_de_endereços:
+                                            log.write (f'{endereços}\n')
+                            else:
+                                socket_comunicação.send('500'.encode())
+                            
+                        else:
+                            socket_comunicação.send('221'.encode())
+                            socket_comunicação.close()
+                            print (f'conexão encerrada com {endereço}')
+                            break
+                    else:
+                        socket_comunicação.close()
+                        print (f'conexão encerrada com {endereço}')
+                        break
+            except Exception as erro:
+                print(f'Um erro ocorreu: {erro}')
 
 servidor=Honeypot()
 
