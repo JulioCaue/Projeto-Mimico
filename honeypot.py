d25de057ee6411926f0292f693af84f44adf5d33
JulioCaue <juliocaue22@gmail.com>
2025-11-30 18:49:21 -0300

    Versão melhorada que consegue suportar multiplas conexões ao mesmo tempo.

--- honeypot.py
+++ honeypot.py
@@ -1,5 +1,7 @@
 import socket
 import datetime 
+import threading
+import time
 
 #Cria classe honeypot, defininindo os parametros que ela começa.
 class Honeypot:
@@ -9,91 +11,113 @@ class Honeypot:
         self.servidor=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.servidor.bind((self.__host,self.__porta))
         self.servidor.listen()
+        self.conexões_ativas=0
+
+
     #define como o honeypot roda
-    def rodar_honeypot(self):
-        #cria o loop externo
-        while  True:
-            #espera conexão até alguém se conectar
-            print('Esperando Conexão...')
-            socket_comunicação,endereço=self.servidor.accept()
-            usuario_tentado=False
-            senha_tentada=False
-            print(f'conexão estabelecida com endereço {endereço}.')
-            socket_comunicação.send('220 welcome'.encode())
-            pass
-
-            try:
-                while True: #recebe e checa o conteudo da mensagem
-                    recebida=socket_comunicação.recv(1024).decode().strip()
-                    if not recebida:
-                        socket_comunicação.close()
-                        break
-                        
-                    else: #age de acordo com o conteudo
-                        if recebida!=('quit'):
-
-
-                            #se user for  o primeiro comando do loop, continua normalmente
-                            if recebida.startswith('user'):
-                                usuario_tentado=recebida.replace('user',' ').strip()
-                                socket_comunicação.send('331'.encode())
-
-                                #Se pass for o segundo comando do loop, envia as informações para o log
-                                if recebida.startswith('pass'):
-                                    senha_tentada=recebida.replace('pass',' ').strip()
-                                    socket_comunicação.send('530'.encode())
-                                    data_formatada = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                                    ip_limpo = endereço[0]
-                                    if usuario_tentado != False and usuario_tentado != False:
-                                        acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado}] - PASS: {senha_tentada}'
-                                        with open ('Honeypot.log','a') as log:
-                                            log.write (f'{acessos}\n')
-
-                                    #Se por algum motivo não houver senha, ele envia  apenas o usuario tentado.
-                                    elif senha_tentada is False:
-                                        acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado} - [Sem senha tentada]'
-                                        with open ('Honeypot.log','a') as log:
-                                            log.write (f'{acessos}\n')
-                                        socket_comunicação.close()
-                                        print (f'conexão encerrada com {endereço}')
-                                        break
-
-                                    #Qualquer outra coisa  fecha o loop interno.
-                                    else:
-                                        socket_comunicação.close()
-                                        print (f'conexão encerrada com {endereço}')
-                                        break
-
-                                #Se user não for o primeiro comando do loop, da erro e volta o loop.
-                                else:
-                                    socket_comunicação.send('500'.encode())
-
-                            #se pass for o primeiro comando do loop, da erro e reseta.
-                            elif recebida.startswith('pass'):
-                                socket_comunicação.send('530'.encode())
-
-                        
-                            else:
-                                socket_comunicação.send('500'.encode())
-                            
-                        else:
-                            socket_comunicação.send('221'.encode())
-                            socket_comunicação.close()
-                            print (f'conexão encerrada com {endereço}')
-                            break
-
-            # Mostra o erro  caso ocorra
-            except Exception as erro:
-                #lida com erro caso usuario desconecte.
-                if erro==('Um erro ocorreu: [WinError 10054] Foi forçado o cancelamento de uma conexão existente pelo host remoto'):
-                    acessos=f'[{data_formatada}] {ip_limpo} - Usuario cancelou a conexão. - USER: {usuario_tentado} - PASS: {senha_tentada}'
-                    with open ('Honeypot.log','a') as log:
-                        log.write (f'{acessos}\n')
+    def interagir_com_cliente(self,socket_comunicação,endereço):
+        usuario_tentado='null'
+        senha_tentada='null'
+        #recebe e checa o conteudo da mensagem   
+        try:
+            while True: 
+                senha_tentada='null'
+                recebida=socket_comunicação.recv(1024).decode().strip()
+                if not recebida:
                     socket_comunicação.close()
-                    print (f'conexão encerrada com {endereço}')
+                    self.conexões_ativas-=1
                     break
-                else:
-                    print(f'Um erro ocorreu: {erro}')
+                    
+                else: #age de acordo com o conteudo
+                    if recebida!=('quit'):
+                        #se user for  o primeiro comando do loop, continua normalmente
+                        if recebida.startswith('user'):
+                            usuario_tentado=recebida.replace('user',' ').strip()
+                            socket_comunicação.send('331'.encode())
+
+                        #Se pass for o segundo comando do loop, envia as informações para o log
+                        elif recebida.startswith('pass'):
+                            if usuario_tentado=='null':
+                                socket_comunicação.send('500'.encode())
+                            else:
+                                senha_tentada=recebida.replace('pass',' ').strip()
+                                socket_comunicação.send('530'.encode())
+                                data_formatada = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                                ip_limpo = endereço[0]
+                                if usuario_tentado != 'null' and senha_tentada != 'null':
+                                    acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado}] - PASS: {senha_tentada}'
+                                    with open ('Honeypot.log','a') as log:
+                                        log.write (f'{acessos}\n')
+                                        usuario_tentado='null'
+                                        senha_tentada='null'
+
+                            #Se por algum motivo não houver senha, ele envia  apenas o usuario tentado.
+                                elif senha_tentada =='null':
+                                    acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado} - [Sem senha tentada]'
+                                    with open ('Honeypot.log','a') as log:
+                                        log.write (f'{acessos}\n')
+                                        usuario_tentado='null'
+                                        senha_tentada='null'
+                                    socket_comunicação.close()
+                                    self.conexões_ativas-=1
+                                    print (f'conexão encerrada com {endereço}')
+                                    break
+
+                                #Qualquer outra coisa  fecha o loop interno.
+                                else:
+                                    socket_comunicação.close()
+                                    self.conexões_ativas-=1
+                                    print (f'conexão encerrada com {endereço}')
+                                    break
+
+
+                    
+                        else:
+                            socket_comunicação.send('500'.encode())
+                        
+                    else:
+                        if senha_tentada=='null':
+                            acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado} - [Sem senha tentada]'
+                            with open ('Honeypot.log','a') as log:
+                                log.write (f'{acessos}\n')
+                                usuario_tentado='null'
+                                senha_tentada='null'
+                        socket_comunicação.send('221'.encode())
+                        socket_comunicação.close()
+                        self.conexões_ativas-=1
+                        print (f'conexão encerrada com {endereço}')
+
+                        break
+
+        # Mostra o erro  caso ocorra
+        except Exception as erro:
+            data_formatada = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            ip_limpo = endereço[0]
+            acessos=f'[{data_formatada}] {ip_limpo} - Usuario cancelou a conexão. - USER: {usuario_tentado} - PASS: {senha_tentada}'
+            with open ('Honeypot.log','a') as log:
+                log.write (f'{acessos}\n')
+            socket_comunicação.close()
+            self.conexões_ativas-=1
+            print(f'Um erro ocorreu: {erro}')
+            print (f'conexão encerrada com {endereço}')
+
+
+
+
+    def rodar_honeypot(self):
+        while True:
+            if self.conexões_ativas<=3:
+                print('Esperando Conexão...')
+                socket_comunicação,endereço=self.servidor.accept()
+                print(f'conexão estabelecida com endereço {endereço}.')
+                socket_comunicação.send('220 welcome'.encode())
+                t=threading.Thread(target=self.interagir_com_cliente,args=(socket_comunicação,endereço),daemon=True)
+                t.start()
+                self.conexões_ativas+=1
+            else:
+                print ('Maximo de conexões ativas foi atingido. Esperando liberação...')
+                time.sleep(10)
+
 
 #dá uma variavel ao honeypot
 servidor=Honeypot()
