e3eea340285c2a78814c71d70b2e53842569e486
JulioCaue <juliocaue22@gmail.com>
2025-12-01 00:38:51 -0300

    Bug de gerenciamento de conexões concertado, agora de fato segura o numero designinado de conexões. gitignore foi apenas revertido ao commit anterior.

--- honeypot.py
+++ honeypot.py
@@ -3,39 +3,32 @@ import datetime
 import threading
 import time
 
-#Cria classe honeypot, defininindo os parametros que ela começa.
 class Honeypot:
     def __init__(self):
         self.__host='localhost'
         self.__porta=2121
+        self.conexões_ativas=0
         self.servidor=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.servidor.bind((self.__host,self.__porta))
         self.servidor.listen()
-        self.conexões_ativas=0
 
-
-    #define como o honeypot roda
     def interagir_com_cliente(self,socket_comunicação,endereço):
         usuario_tentado='null'
         senha_tentada='null'
-        #recebe e checa o conteudo da mensagem   
+        socket_comunicação.send('220 welcome'.encode())   
         try:
             while True: 
                 senha_tentada='null'
                 recebida=socket_comunicação.recv(1024).decode().strip()
                 if not recebida:
-                    socket_comunicação.close()
                     self.conexões_ativas-=1
+                    socket_comunicação.close()
                     break
-                    
-                else: #age de acordo com o conteudo
+                else:
                     if recebida!=('quit'):
-                        #se user for  o primeiro comando do loop, continua normalmente
                         if recebida.startswith('user'):
                             usuario_tentado=recebida.replace('user',' ').strip()
                             socket_comunicação.send('331'.encode())
-
-                        #Se pass for o segundo comando do loop, envia as informações para o log
                         elif recebida.startswith('pass'):
                             if usuario_tentado=='null':
                                 socket_comunicação.send('500'.encode())
@@ -50,33 +43,19 @@ class Honeypot:
                                         log.write (f'{acessos}\n')
                                         usuario_tentado='null'
                                         senha_tentada='null'
-
-                                #Qualquer outra coisa  fecha o loop interno.
                                 else:
-                                    socket_comunicação.close()
                                     self.conexões_ativas-=1
+                                    socket_comunicação.close()
                                     print (f'conexão encerrada com {endereço}')
                                     break
-
-
-
                         else:
                             socket_comunicação.send('500'.encode())
-                        
                     else:
-                        if senha_tentada=='null':
-                            acessos=f'[{data_formatada}] {ip_limpo} - USER: {usuario_tentado} - [Sem senha tentada]'
-                            with open ('Honeypot.log','a') as log:
-                                log.write (f'{acessos}\n')
-                                usuario_tentado='null'
-                                senha_tentada='null'
                         socket_comunicação.send('221'.encode())
-                        socket_comunicação.close()
                         self.conexões_ativas-=1
+                        socket_comunicação.close()
                         print (f'conexão encerrada com {endereço}')
                         break
-
-        # Mostra o erro  caso ocorra
         except Exception as erro:
             if senha_tentada!='null':
                 data_formatada = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -84,32 +63,25 @@ class Honeypot:
                 acessos=f'[{data_formatada}] {ip_limpo} - Usuario cancelou a conexão. - USER: {usuario_tentado} - PASS: {senha_tentada}'
                 with open ('Honeypot.log','a') as log:
                     log.write (f'{acessos}\n')
-            socket_comunicação.close()
             self.conexões_ativas-=1
+            socket_comunicação.close()
             print(f'Um erro ocorreu: {erro}')
             print (f'conexão encerrada com {endereço}')
 
-
-
-
     def rodar_honeypot(self):
         while True:
-            if self.conexões_ativas<=3:
+            if self.conexões_ativas<3:
                 print('Esperando Conexão...')
                 socket_comunicação,endereço=self.servidor.accept()
-                print(f'conexão estabelecida com endereço {endereço}.')
-                socket_comunicação.send('220 welcome'.encode())
+                self.conexões_ativas+=1
+                print(f'conexão estabelecida com endereço {endereço}. {self.conexões_ativas} de 3 estão ativas.')
                 t=threading.Thread(target=self.interagir_com_cliente,args=(socket_comunicação,endereço),daemon=True)
                 t.start()
-                self.conexões_ativas+=1
             else:
+                socket_comunicação,endereço=self.servidor.accept()
                 socket_comunicação.send("421 Too many users, try again later\r\n".encode())
+                socket_comunicação.close()
                 print ('Maximo de conexões ativas foi atingido. Esperando liberação...')
-                time.sleep(10)
 
-
-#dá uma variavel ao honeypot
 servidor=Honeypot()
-
-#Inicia o sercidor
 servidor.rodar_honeypot()
