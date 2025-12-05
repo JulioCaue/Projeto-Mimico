00a4eb2338cbc53db75de6560b505054e4572852
JulioCaue <juliocaue22@gmail.com>
2025-12-05 17:34:31 -0300

    Adicionado classe, função iniciadora, comando list.

--- Fake_FileSystem_logica.py
+++ Fake_FileSystem_logica.py
@@ -1,1 +1,17 @@
-#será feito em breve :)
+class Logica_de_arquivos():
+    def __init__(self):
+        import Fake_FileSystem_dados as filesystem
+        self.filesystem=filesystem.dados
+        self.diretorio_atual=self.filesystem['/']
+
+    def comando_list(self):
+        lista_de_arquivos=[]
+        for arquivos in self.diretorio_atual:
+            lista_de_arquivos.append(arquivos)
+        print (' '.join(lista_de_arquivos))
+        lista_de_arquivos=[]
+
+
+teste=Logica_de_arquivos()
+
+teste.comando_list()
