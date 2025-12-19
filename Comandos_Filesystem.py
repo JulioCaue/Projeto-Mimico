078988e815487e7431afc6f7cc6d7e23662292d7
JulioCaue <juliocaue22@gmail.com>
2025-12-18 23:12:00 -0300

    pequenas mudanças de espaçamento em geral, logica de caracteres proibidos refeita no mkdir, interação com cliente inserida na função stor.

--- Comandos_Filesystem.py
+++ Comandos_Filesystem.py
@@ -1,3 +1,5 @@
+import socket
+
 class Logica_de_arquivos():
     def __init__(self):
         import Pastas_filesystem as filesystem
@@ -9,90 +11,108 @@ class Logica_de_arquivos():
         self.caracteres_proibidos = "'\"><,:?*/|\\"
 
     #Lista arquivos no diretorio atual. Simples
-    def comando_list(self):
+    def list(self):
         lista_de_arquivos=[]
         for arquivo in self.diretorio_atual:
             lista_de_arquivos.append(arquivo)
-        return (' '.join(lista_de_arquivos))
+        if not lista_de_arquivos:
+            return None
+        else: return (' '.join(lista_de_arquivos))
         
     #Diretorio antigo é o diretorio atual -> diretorio atual vira diretorio antigo + diretorio novo.
     #'cd ' é removido antes de fazer operações com conteudo.
-    def comando_cd(self,diretorio_novo):
-        #retira cd do input recebido
-        comando_separado=diretorio_novo.split()
-        if len(comando_separado) < 2:
-            return
-        diretorio_do_comando=comando_separado[1:]
-        diretorio_novo=' '.join(diretorio_do_comando)
-
+    def cd(self,diretorio_novo,socket_comunicação):
         #verifica se está voltando ou avançando.
         if diretorio_novo == '..':
             #volta para pasta anterior
             if not self.caminho_atual:
-                return ('erro')
+                return (b'erro')
             else:
                 self.caminho_atual.pop()
                 self.diretorio_atual=self.filesystem['/']
                 for pasta in self.caminho_atual:
                     self.diretorio_atual=self.diretorio_atual[pasta]
+                socket_comunicação.send(b"250 Directory successfully changed.\r\n")
 
         #avança para a proxima pasta se for uma pasta e não arquivo
         elif diretorio_novo in self.diretorio_atual:
             conteudo = self.diretorio_atual[diretorio_novo]
+            socket_comunicação.send(b"250 Directory successfully changed.\r\n")
             if isinstance(conteudo, dict):
                 self.diretorio_atual=conteudo
                 self.caminho_atual.append(diretorio_novo)
             else:
-                print ('erro: é arquivo')
+                return (b'550 Not a directory.\r\n')
         else:
-            return ('erro: não existe')
+            return (b'550 File or directory not found.\r\n')
 
     #Lista caminho atual
-    def comando_pwd(self):
+    def pwd(self):
         return ('/' + '/'.join(self.caminho_atual))
 
     
     #Envia arquivo ao cliente
-    def comando_RETR(self, arquivo_requisitado):
-        #retira o RETR do comando
-        comando_separado=arquivo_requisitado.split(' ',1)
-        if len(comando_separado) < 2:
-            return ('erro: falta nome do arquivo')
-        arquivo_do_comando=comando_separado[1:]
-        arquivo_requisitado=''.join(arquivo_do_comando)
-
+    def retr(self, arquivo_requisitado):
         #verifica se o arquivo está na pasta. Se estiver, envia o conteudo. Se não, da erro.
         if arquivo_requisitado in self.diretorio_atual:
             conteudo_do_arquivo=self.diretorio_atual[arquivo_requisitado]
             if isinstance(conteudo_do_arquivo,dict):
-                return ('erro: Não é arquivo')
+                return (b'550 File or directory not found.\r\n')
             else:
                 return conteudo_do_arquivo
         else:
-            return ('erro: arquivo não encontrado.')
+            return (b'550 File or directory not found.\r\n')
     
     #grava arquivo no dicionario como nomes.
-    def logica_comando_STOR(self,nome_virus_recebido,bytes_virus_recebido):
+    def stor(self,socket_comunicação,comando_recebido):
+        #envia sinal de confimação
+        socket_comunicação.send(b'150 Opening data connection.')
+        #pega nome do virus"
+        comando_recebido.split()
+        comando_separado=comando_recebido[1:]
+        comando_separado=comando_separado[1]
+        comando_recebido=' '.join(comando_separado)
+        nome_virus_recebido=comando_recebido
+
         if nome_virus_recebido in self.diretorio_atual:
-            return ('já existe')
+            return (b'550 Requested action not taken')
+
+        if not nome_virus_recebido:
+            socket_comunicação.send(b'')
+        bytes_virus_recebido=bytearray()
+
+        terminado=False
+        while terminado==False:
+            data=socket_comunicação.recv(1024)
+            if not data:
+                terminado=True
+            else:
+                bytes_virus_recebido+=data
+
+        with open(f'quarentena/{nome_virus_recebido}.quarentena','wb') as arquivo:
+            arquivo.write(bytes_virus_recebido)
+            arquivo.close()
+
+        socket_comunicação.send(b'226 Closing data connection')
+
         self.diretorio_atual[nome_virus_recebido]=bytes_virus_recebido
 
 
     #cria um diretorio novo
-    def comando_mkdir(self,nome_pasta_nova):
-        #Retira "mkdir" do input recebido
-        comando_separado=nome_pasta_nova.split()
-        nome_pasta_nova=comando_separado[1:]
-        nova_pasta=' '.join(nome_pasta_nova)
-
+    def mkdir(self,nova_pasta):
         for caractere in nova_pasta:
             if caractere in self.caracteres_proibidos or caractere == (' ',''):
-                return ('contem caracteres proibidos.')
+                return (b'553 File name not allowed.\r\n')
+            
+        for caractere in nova_pasta:
+            if caractere in [".",'..','']:
+                return (b'553 File name not allowed.\r\n')
         if nova_pasta in self.diretorio_atual:
-            return ('pasta já existe')
+            return (b'550 Directory already exists.\r\n')
         elif not nova_pasta:
             #adiciona nova key (vazia) ao filesystem (AKA nova pasta).
             self.diretorio_atual[nova_pasta]={}
+            return (b'257 Directory created.\r\n')
 
 
 
@@ -107,21 +127,21 @@ diretorio2='shadow'''
 
 teste=Logica_de_arquivos()
 
-while True:
-    teste.comando_list()
+'''while True:
+    teste.list()
     diretorio_novo=input('comando aqui: ')
     if diretorio_novo.startswith('mkdir'):
         nome_pasta_nova=diretorio_novo
-        teste.comando_mkdir(nome_pasta_nova)
+        teste.mkdir(nome_pasta_nova)
     else:
-        teste.comando_cd(diretorio_novo)
-    print (teste.comando_list())
+        teste.cd(diretorio_novo)
+    print (teste.list())
 
 
 #diretorio3='shadow'
 teste.comando_cd(diretorio_novo)
 #teste.comando_list()
-teste.comando_pwd()
+teste.comando_pwd()'''
 
 
 
