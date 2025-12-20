eb68d48902c20ac852fb92dbb5b8e255fd58a127
JulioCaue <juliocaue22@gmail.com>
2025-12-20 16:48:02 -0300

    correções de bugs após testes com novo arquivo invasor.

--- honeypot.py
+++ honeypot.py
@@ -17,7 +17,7 @@ class Honeypot:
         comando_separado=comando_recebido.split(' ',1)
         if len(comando_separado)>1:
             comando_recebido=comando_separado[1]
-            return comando_recebido
+            return comando_recebido.strip()
         else:
             socket_comunicação.send(b'501 Syntax error in parameters or arguments.\r\n')
             return ''
@@ -26,12 +26,14 @@ class Honeypot:
         #Cria variavel comando para usar comandos e envia mensagem inivial ao invasor.
         comando=fs.Logica_de_arquivos()
         socket_comunicação.send(b'220 welcome\r\n')
+        # Adicionado tamanho do buffer
         comando_recebido=socket_comunicação.recv(1024).decode()
 
         #sistema basico de login para enganar
         if comando_recebido.startswith('user'):
             socket_comunicação.send(b'331 Please specify the password.\r\n')
         else: socket_comunicação.send(b'530 Not logged in.\r\n')
+        # Adicionado tamanho do buffer
         comando_recebido=socket_comunicação.recv(1024).decode()
         if comando_recebido.startswith('pass'):
             socket_comunicação.send(b'230 Login successful.')
@@ -39,14 +41,24 @@ class Honeypot:
 
 
         while True:
-            comando_recebido=socket_comunicação.recv(1024).decode().lower()
+            # CORREÇÃO CRÍTICA AQUI: recv() vazio causava erro. Adicionado 1024 e strip()
+            try:
+                dados_raw = socket_comunicação.recv(1024)
+                if not dados_raw: break # Se cliente desconectar, encerra loop
+                comando_recebido = dados_raw.decode().strip().lower()
+            except:
+                break
 
             if comando_recebido.startswith('list'):
-                socket_comunicação.send(f'{comando.list()}'.encode())
+                resp = comando.list()
+                if resp:
+                    socket_comunicação.send(f'{resp}'.encode())
+                else:
+                    socket_comunicação.send(b'') # Envia vazio se nada listar
 
             elif comando_recebido.startswith('cd'):
                 diretorio_novo=self.separar_comando(comando_recebido,socket_comunicação)
-                socket_comunicação.send(comando.cd(diretorio_novo,socket_comunicação))
+                socket_comunicação.send(comando.cd(diretorio_novo))
 
             elif comando_recebido.startswith('pwd'):
                 socket_comunicação.send(f'{comando.pwd()}'.encode())
@@ -61,14 +73,19 @@ class Honeypot:
             
             elif comando_recebido.startswith('mkdir'):
                 nova_pasta=self.separar_comando(comando_recebido,socket_comunicação)
-                comando.mkdir(nova_pasta)
+                socket_comunicação.send(comando.mkdir(nova_pasta)) # Adicionado send que faltava
             
             elif comando_recebido.startswith('stor'):
-                comando.stor(socket_comunicação,comando_recebido)
+                comando_recebido=self.separar_comando(comando_recebido,socket_comunicação)
+                socket_comunicação.send(comando.stor(socket_comunicação,comando_recebido))
 
             elif comando_recebido=='quit':
-                socket_comunicação.send(b'221 Goodbye')
-                socket_comunicação.close()
+                try:
+                    socket_comunicação.send(b'221 Goodbye')
+                except ConnectionError:
+                    print ('conexão cortada pelo cliente.')
+                    socket_comunicação.close()
+                    break # Sai do loop
 
             else:
                 socket_comunicação.send(b'502 Command not implemented.\r\n')
