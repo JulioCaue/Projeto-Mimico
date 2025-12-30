107261eb32692e073b5768537a990b4bd7ac3f5e
JulioCaue <juliocaue22@gmail.com>
2025-12-30 16:51:39 -0300

    Arquivo pequeno que cria o banco de dados caso não exista. Roda automaticamente no arquivo honeypot principal.

---
+++ criar_banco_de_dados.py
@@ -0,0 +1,34 @@
+import os
+import sqlite3
+
+def criar_banco():
+    if not os.path.exists('coletor.db'):
+        with sqlite3.connect('coletor.db') as conexão:
+            cursor=conexão.cursor()
+            cursor.execute('''
+                create table sessões(
+                        ID_de_usuario integer primary key autoincrement,
+                        IP_de_origem text,
+                        Porta_de_origem integer,
+                        Data_de_inicio text,
+                        Data_de_finalização text)
+            ''')
+            cursor.execute('''
+                create table comandos(
+                        ID_do_comando integer primary key autoincrement,
+                        ID_de_usuario integer,
+                        Comando text,
+                        Argumento text,
+                        Timestamp text,
+                        Foreign key (ID_de_usuario) REFERENCES sessões(ID_de_usuario))
+            ''')
+            cursor.execute('''
+                create table capturas(
+                        ID_da_captura integer primary key autoincrement,
+                        ID_de_usuario integer,
+                        Nome_do_arquivo text,
+                        Tamanho_do_arquivo integer,
+                        Hash_do_arquivo text,
+                        Foreign key (ID_de_usuario) REFERENCES sessões(ID_de_usuario))
+        ''')
+        cursor.close()
